69c373d49e3cbd129238945aa484f3a2399b0a8f
Kaio Mazza <mazza.kaio12@gmail.com>
2025-02-12 19:53:50 -0300

    Update exe02_KaioMazza.py

--- exe02_KaioMazza.py
+++ exe02_KaioMazza.py
@@ -1,7 +1,7 @@
 # Peça ao usuário para inserir um número inferior a 20. Se ele inserir um número 20 ou mais, 
 # exiba a mensagem "Muito alto", caso contrário, exiba "Obrigado".
 
-num = float(input("Digite um número inferior a 20:"))
+num = float(input("Digite um número inferior a 20: "))
 if num >= 20:
     print("Número inserido MUITO ALTO!")
 else:
