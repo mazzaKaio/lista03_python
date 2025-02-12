32b2c40c1312ea6f9abb1058321ef1dd58a8be45
Kaio Mazza <mazza.kaio12@gmail.com>
2025-02-11 21:36:03 -0300

    Resolucao exe. 02

---
+++ exe02.py
@@ -0,0 +1,8 @@
+# Peça ao usuário para inserir um número inferior a 20. Se ele inserir um número 20 ou mais, 
+# exiba a mensagem "Muito alto", caso contrário, exiba "Obrigado".
+
+num = float(input("Digite um número inferior a 20:"))
+if num >= 20:
+    print("Número inserido MUITO ALTO!")
+else:
+    print("Obrigado!")
