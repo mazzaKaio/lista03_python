6c99ef2114ee461ad5af28c69323001b2ff84399
Kaio Mazza <mazza.kaio12@gmail.com>
2025-02-12 19:39:58 -0300

    Resolucao exe. 11

---
+++ exe11.py
@@ -0,0 +1,11 @@
+# Escreva um programa que pergunte a distância que um passageiro deseja percorrer em km. 
+# Calcule o preço da passagem, cobrando R$ 0,50 por km para viagens de até de 200 km, e R$ 0,45 para viagens mais longas.
+
+distanciaDesejada = float(input("Insira o quando (em KM) você deseja percorrer:"))
+
+if distanciaDesejada < 200:
+    precoPassagem = distanciaDesejada * 0.5
+else:
+    precoPassagem = distanciaDesejada * 0.45
+
+print("Sua viagem irá custar: R$", precoPassagem)
