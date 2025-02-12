dd8ce9091f12f3bc884b632683664bcb67147569
Kaio Mazza <mazza.kaio12@gmail.com>
2025-02-12 19:31:20 -0300

    Edicao exe. 9

--- exe09.py
+++ exe09.py
@@ -7,21 +7,17 @@ if rendaMensal <= 2259.20:
 else:
     if rendaMensal >= 2259.21 and rendaMensal <= 2826.65:
         aliquota = 0.075
-        imposto = rendaMensal * aliquota
-        print("Seu imposto conforme a tabela de 2025 é: R$", imposto)
     else:
         if rendaMensal >= 2826.66 and rendaMensal <= 3751.05:
             aliquota = 0.15
-            imposto = rendaMensal * aliquota
-            print("Seu imposto conforme a tabela de 2025 é: R$", imposto)
         else:
             if rendaMensal >= 3751.06 and rendaMensal <= 4664.68:
                 aliquota = 0.225
-                imposto = rendaMensal * aliquota
-                print("Seu imposto conforme a tabela de 2025 é: R$", imposto)
             else:
                 if rendaMensal >= 4664.68:
                     aliquota = 0.275
-                    imposto = rendaMensal * aliquota
-                    print("Seu imposto conforme a tabela de 2025 é: R$", imposto)
+                
+imposto = rendaMensal * aliquota
+print("Seu imposto conforme a tabela de 2025 é: R$", imposto)
+
 print("'Kaio Gomes do Nascimento Mazza'")
---
+++ exe10.py
@@ -0,0 +1,9 @@
+# Escreva um programa que pergunte o salário do funcionário e calcule o valor do aumento. 
+# Para salários superiores a R$ 1.250,00, calcule um aumento de 10%. Para os inferiores ou iguais, de 15%.
+
+salario = float(input("Digite seu salário: "))
+
+if salario > 1250:
+    aumento = 0.10
+    
+print("'Kaio Gomes do Nascimento Mazza'")
