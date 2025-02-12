cc12fd82c7ca6c91f308291871c3b45a8a3afa81
Kaio Mazza <mazza.kaio12@gmail.com>
2025-02-12 19:46:24 -0300

    resolucao exe. 12

---
+++ exe12.py
@@ -0,0 +1,26 @@
+# Escreva um programa que leia dois números e que pergunte qual operação você deseja realizar. 
+# Você deve poder calcular a soma (+), subtração (-), multiplicação (*) e divisão (/). 
+# Exiba o resultado da operação solicitada. (usar ELIF)
+
+num1 = int(input("Digite o primeiro número: "))
+operacao = str(input("Digite qual operação você deseja usar (+), (-), (*), (/): "))
+num2 = int(input("Digite o segundo número: "))
+
+if operacao.__eq__("+"):
+    resultado = num1 + num2
+    print(num1, "+", num2, "=", resultado)
+
+elif operacao.__eq__("-"):
+    resultado = num1 - num2
+    print(num1, "-", num2, "=", resultado)
+
+elif operacao.__eq__("*"):
+    resultado = num1 * num2
+    print(num1, "*", num2, "=", resultado)
+
+elif operacao.__eq__("/"):
+    resultado = num1 / num2
+    print(num1, "/", num2, "=", resultado)
+
+else:
+    print("Operação não aceita!")
