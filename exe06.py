a4ca588a50462fad6f12ad8f6492f64f97cc0d8e
Kaio Mazza <mazza.kaio12@gmail.com>
2025-02-11 22:00:13 -0300

    Resolucao exe. 06

---
+++ exe06.py
@@ -0,0 +1,17 @@
+# Pergunte a idade do usuário. Se tiver 16 anos ou mais, exiba a mensagem "Você pode votar", 
+# se tiver 18 anos, exiba a mensagem "Você pode aprender a dirigir", 
+# se tiver 14 anos, exiba a mensagem "Você pode comprar um bilhete de loteria", 
+# se tiver menos de 14 anos, exiba a mensagem "Você pode fazer doces ou travessuras".
+
+idade = int(input("Digite sua idade: "))
+
+if idade >= 18:
+    print("Você pode aprender a dirigir!")
+else:
+    if idade >= 16:
+        print("Você pode votar!")
+    else:
+        if idade == 14:
+            print("Você pode comprar um bilhete de loteria!")
+        else:
+            print("Você pode fazer doces ou travessuras!")
